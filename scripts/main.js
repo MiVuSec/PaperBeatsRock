a8e5b56a495f47aaacd53de03e5fb50411e5377e
MiVuSec <>
2025-07-18 20:14:37 -0400

    edited main and readme

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+let startTime = new Date();
+let endTime = new Date();
+
 fetch('https://api.coinlore.net/api/global/')
-	.then(response => response.json())
-	.then(json => console.log(json));
+.then(response => response.json())
+.then(json => {
+	endTime = new Date();
+	console.log(json, `\n${endTime - startTime}ms`);
+	return;
+});
+
+let timeFrameMilliSeconds = 0;
+let promises = new Array<Promise>();
+(async () => {
+	while (true) {
+		console.log (
+			
+			await fetch('https://api.coinlore.net/api/global/')
+			.then(response => response.json()),
+			new Date() - time
+		);
+		time = new Date();
+		//await new Promise(resolve => setTimeout(resolve, 2000));
+	}
+})();
